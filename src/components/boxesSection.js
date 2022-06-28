15cb4c682a4d6127ad11893ce68a2ccb5f4f54b2
Fabricio <contact@fabricioguardia.com>
2022-06-28 14:14:07 -0700

    activities section

--- src/components/boxesSection.js
+++ src/components/boxesSection.js
@@ -4,7 +4,13 @@ const BoxSection = ({ activity }) => {
   
   return (
     <div className="boxSection-item">
-      Hello
+      <div className="boxSection-item-image">
+        <img src={activity.image} alt={activity.item} />
+      </div>
+      <div className="boxSection-item-description">
+        <h1>{activity.item}</h1>
+        <button>Learn more</button>
+      </div>
     </div>
   )
 }
--- src/helpers/activities.js
+++ src/helpers/activities.js
@@ -1,11 +1,38 @@
-export const activities = [
-  'hiking',
-  'biking',
-  'vistas',
-  'picnics',
-  'stargazing',
-  'art & culture',
-  'spiritual',
-  'secret 7 map',
-  'sedona secret season',
+export const activities = [ 
+  { 
+    item: 'hiking',
+    image: 'https://visit-sedona.s3.amazonaws.com/CMS/13970/brc09825_aerie__small.jpg'
+  },
+  {
+    item: 'biking',
+    image: 'https://visit-sedona.s3.amazonaws.com/CMS/13973/brc09881__small.jpg'
+  },
+  {
+    item: 'vistas',
+    image: 'https://visit-sedona.s3.amazonaws.com/CMS/14010/brc09723__small.jpg'
+  },
+  {
+    item: 'picnics',
+    image: 'https://visit-sedona.s3.amazonaws.com/CMS/13981/brc09217__1280x720__small.jpg'
+  },
+  {
+    item: 'stargazing',
+    image: 'https://visit-sedona.s3.amazonaws.com/CMS/14018/brc00724_copy_2000x1333__small.jpg'
+  },
+  {
+    item: 'art & culture',
+    image: 'https://visit-sedona.s3.amazonaws.com/CMS/14026/brc00435__small.jpg'
+  },
+  {
+    item: 'spiritual',
+    image: 'https://visit-sedona.s3.amazonaws.com/CMS/14034/brc00546__small.jpg'
+  },
+  {
+    item: 'secret 7 map',
+    image: 'https://visit-sedona.s3.amazonaws.com/CMS/13983/secret7_map__small.jpg'
+  },
+  {
+    item: 'sedona secret season',
+    image: 'https://visit-sedona.s3.amazonaws.com/CMS/14237/img_2830_copy_1200x800__small.jpg'
+  }
 ]
